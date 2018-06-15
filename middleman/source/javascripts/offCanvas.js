ef819b7bc9d04ac9e5641e6f6998c9f4cad7ca19
Damjack <giacomello.damiano@gmail.com>
2018-06-15 16:42:24 +0200

    Remove middleman example app and old_docs

--- middleman/source/javascripts/offCanvas.js
+++
@@ -1,9 +0,0 @@
-const $ = require('jquery')
-
-$(document).ready(function() {
-  $('.toggle-menu').on('click touchstart',  function(e) {
-    e.preventDefault();
-    $('.menu').toggleClass('is-visible');
-    $('.content__overflow').toggleClass('hidden');
-  });
-});
