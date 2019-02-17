bcb8480dfafc798032a1c6235949097601d03ddf
damjack <giacomello.damiano@gmail.com>
2019-02-17 23:17:00 +0100

    Add jekyll site

--- docs/components/Base.js
+++
@@ -1,9 +0,0 @@
-import { Main } from './Main'
-import { Title } from './Title'
-import { Button } from './Button'
-import { Preview } from './Preview'
-import { Code } from './Code'
-import { Block } from './Block'
-import { Table } from './Table'
-
-export { Main, Title, Button, Preview, Code, Block, Table }
--- docs/components/Title.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const Title = ({ children, ...props }) => {
-  return (<h2 {...props}>{children}</h2>);
-}
-
-export { Title };
