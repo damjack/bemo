506e3c3aa188737e3b724b87dbb268bdee3f2ebc
damjack <giacomello.damiano@gmail.com>
2019-04-04 14:49:05 +0200

    Update jekyll site

--- docs/source/index.md
+++ docs/source/index.md
@@ -1,6 +1,24 @@
 ---
-# Feel free to add content and custom Front Matter to this file.
-# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
-
 layout: default
 ---
+
+<header class="hero--purple">
+  <div class="wrap--small">
+    <div class="hero__body padding--both-2">
+      <div class="hero__body__inner">
+        <h1 class="hero__heading">Bemo</h1>
+        <p class="hero__text">BEMO is a Sass-based project starter meant to be a starting point and structural guide rather than a full framework. It's bare enough to not interfere with your custom styles or scripts, but yet has enough functioning modules to jumpstart a project in no time</p>
+      </div>
+    </div>
+  </div>
+</header>
+
+<section class="wrap">
+  <div class="grid">
+    <div class="grid--center">
+      <div class="grid__item desk-10-12">
+
+      </div>
+    </div>
+  </div>
+</section>
---
+++ docs/webpack.config.js
@@ -0,0 +1,103 @@
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+
+const extractSass = new ExtractTextPlugin({
+  filename: "css/[name].css",
+});
+
+module.exports = {
+  entry: {
+    application: "./assets/js/index.js",
+    styles: "./assets/sass/application.sass"
+  },
+  resolve: {
+    modules: [path.join(__dirname, "assets/js"), "node_modules"],
+    extensions: [".js", ".jsx"],
+    alias: {
+      modernizr$: path.resolve(__dirname, ".modernizrrc.js")
+    }
+  },
+  output: {
+    path: path.resolve(
+      __dirname,
+      process.env.NODE_ENV === "development"
+        ? "./source/assets"
+        : "./_site/assets"
+    ),
+    filename: "js/[name].js",
+    publicPath: "/assets/"
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: false,
+          compress: true,
+          comments: false
+        }
+      })
+    ],
+  },
+  module: {
+    rules: [
+      {
+        loader: "webpack-modernizr-loader",
+        test: /\.modernizrrc\.js$/
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        enforce: "pre",
+        test: /\.s?css/,
+        use: "import-glob-loader"
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: extractSass.extract({
+          use: [
+            { loader: "css-loader" },
+            {
+              loader: "postcss-loader",
+              options: {
+                plugins: () => [autoprefixer()]
+              }
+            },
+            { loader: "sass-loader" }
+          ],
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    extractSass,
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery", // Used for Bootstrap JavaScript components
+      jQuery: "jquery", // Used for Bootstrap JavaScript components
+    })
+  ]
+};
